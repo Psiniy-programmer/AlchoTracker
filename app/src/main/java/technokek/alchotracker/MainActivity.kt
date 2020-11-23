43811b02a5655eb2a8ccbe91cf2933b3c8161f2d
Dmitry <dmitry.trofimow2011@gmail.com>
2020-11-23 23:03:52 +0300

    Added TimerFragment and its logic

--- app/src/main/java/technokek/alchotracker/MainActivity.kt
+++ app/src/main/java/technokek/alchotracker/MainActivity.kt
@@ -1,11 +1,50 @@
 package technokek.alchotracker
 
+import android.app.NotificationManager
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.fragment.app.FragmentManager
+import technokek.alchotracker.timer.NotificationActivity
+import technokek.alchotracker.timer.TimerFragment
 
 class MainActivity : AppCompatActivity() {
+    private val fragmentManager = supportFragmentManager
+    private lateinit var notificationManager: NotificationManagerCompat
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        notificationManager =
+            NotificationManagerCompat.from(this)
+        addTimerFragment()
+    }
+
+    private fun addTimerFragment() {
+        val timerFragment = TimerFragment()
+        val transaction = fragmentManager
+            .beginTransaction()
+            .replace(R.id.content_parent, timerFragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    fun sendTimerFragmentNotificationChannel1(id:Int, title:String, text:String) {
+        val builder = NotificationCompat.Builder(this, NotificationActivity.CHANNEL_1_ID)
+            .setSmallIcon(R.drawable.ic_baseline_notifications_active_24)
+            .setContentTitle(title)
+            .setContentText(text)
+        val notification = builder.build()
+        notificationManager.notify(id, notification)
+    }
+
+    fun sendTimerFragmentNotificationChannel2(id:Int, title:String, text:String) {
+        val builder = NotificationCompat.Builder(this, NotificationActivity.CHANNEL_2_ID)
+            .setSmallIcon(R.drawable.ic_notification_time_interval)
+            .setContentTitle(title)
+            .setContentText(text)
+        val notification = builder.build()
+        notificationManager.notify(id, notification)
     }
 }
---
+++ app/src/main/java/technokek/alchotracker/timer/State.java
@@ -0,0 +1,8 @@
+package technokek.alchotracker.timer;
+
+public enum State {
+    CHOOSING,
+    RUNNING,
+    PAUSED,
+    INTERRUPTED
+}
