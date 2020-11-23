e752140aec9b6cd126dfbab92e5ff1b16abcd37a
Dimas <psiniy-programmer@yandex.ru>
2020-11-23 09:18:25 +0300

    recycler init

--- app/src/main/java/technokek/alchotracker/ui/fragments/Profile.kt
+++ app/src/main/java/technokek/alchotracker/ui/fragments/Profile.kt
@@ -4,23 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.profile_fragment.*
 import technokek.alchotracker.R
 
-class Profile: Fragment(){
+class Profile : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view =  inflater.inflate(R.layout.profile_fragment, container, false)
-        val recyclerView: RecyclerView = view.findViewById(R.id.profile_preferences_list)
-        recyclerView.setHasFixedSize(true)
-        recyclerView.layoutManager = GridLayoutManager(view.context, 2, RecyclerView.VERTICAL, false)
+        return inflater.inflate(R.layout.profile_fragment, container, false)
+    }
 
-        return view
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recyclerView: RecyclerView = view.findViewById<RecyclerView>(R.id.profile_preferences_list)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager =
+            GridLayoutManager(view.context, 2, RecyclerView.VERTICAL, false)
+
     }
 }
