665cc1a5aaefab9cf6bf659ffc90c4cbd20ffb45
psinka <psiniy-programmer@yandex.ru>
2020-12-28 23:57:30 +0300

    apk 0.1

--- app/src/main/java/technokek/alchotracker/ui/activity/LoginActivity.kt
+++ app/src/main/java/technokek/alchotracker/ui/activity/LoginActivity.kt
@@ -30,7 +30,7 @@ class LoginActivity : AppCompatActivity(), AuthListener, KodeinAware {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        val binding: AccountLoginBinding = DataBindingUtil.setContentView(this, R.layout.account_login)
         viewModel = ViewModelProviders.of(this, factory).get(AuthViewModel::class.java)
         binding.viewmodel = viewModel
         viewModel.authListener = this
